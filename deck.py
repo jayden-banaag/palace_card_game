605cc6127d67f2d6c55111df6786320886e13058
jayden-banaag <jaydencbanaag@gmail.com>
2026-01-15 17:57:18 -0500

    Add files via upload

---
+++ deck.py
@@ -0,0 +1,19 @@
+import random
+
+class Card(object):
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+def create_deck():
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+    deck = [Card(value, suit) for suit in suits for value in values]
+    return deck
+
+def shuffle_deck(deck):
+    random.shuffle(deck)
+    return deck
